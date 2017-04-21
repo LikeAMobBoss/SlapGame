b3a32e7bd1a6d8fbfe383b07a2126d776448212a
Jason Tidwell <tidwell2726@ymail.com>
2017-04-21 16:45:58 -0600

    finished core of game

--- game.js
+++ game.js
@@ -1,26 +1,39 @@
 var healthbar = document.getElementById("healthbar")
 var health = 100
+var hits = 0
 
-function updateScreen(){
-  
-healthbar.innerHTML=health
-}
-
-function slap(){
-health -= 1 
-updateScreen()
-}
-
-
-function normalSlap(){
-health -= 5
-updateScreen()
+function updateScreen() {
+  if (health <= 0) {
+    health = 0
+  }
+  document.getElementById("hits").innerHTML = hits
+  healthbar.innerHTML = health
 }
 
 
-function slapHimSilly(){
-health -= 10
-updateScreen()
+function reset() {
+  health = 100
+  hits = 0
+  updateScreen()
+}
+function slap() {
+  health -= 1
+  hits++
+  updateScreen()
+}
+
+
+function normalSlap() {
+  health -= 5
+  hits++
+  updateScreen()
+}
+
+
+function slapHimSilly() {
+  health -= 10
+  hits++
+  updateScreen()
 }
 
 updateScreen()
